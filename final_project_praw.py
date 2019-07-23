db4126e4b7957699d67aa7ba4923f867856a7cad
Aditi Rajagopal <aditi.rajagopal92@gmail.com>
2019-07-22 22:07:51 -0400

    grab comments from two game threads
    TO DO:
    - Scikit-Learn?
    - Map out semantics over the course of the game
    - 4 Queries

--- final_project_praw.py
+++ final_project_praw.py
@@ -6,46 +6,41 @@ CS5010: Final Project
 Names: Aditi Rajagopal, Bradley Katcher, Charlie Putnam
 Computing-ID: ar5vt, bk5pu, cmp2cz
 Notes: /r/CFB web scraper looking at rivalry data
-TO DO: pull .json data from each post
+TO DO: clean up class, decide what to do with data....Scikit-Learn?
 """
 
-#praw package
-import praw, datetime
+import praw, datetime, json
 import pandas as pd
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-#import creds
-#separate file to store credentials for PRAW, file named creds.py
-
-
-#client_id = creds.client_id
-#client_secret = creds.client_secret
-#user = creds.user
-#password = creds.password
-#user_agent = creds.user_agent
 
 class RedditPostParse: 
 
     def __init__(self, URL, flair1, flair2):
         self.URL = URL
         self.flair1 = flair1
-        self.flair1DF = []
+        #self.flair1DF = []
         self.flair2 = flair2
-        self.flair2DF = []
+        #self.flair2DF = []
         self.postDetails = []
         self.postDF = []
 
     def getComments(self):
         
-        #create Reddit object by passing credentials to Praw
-        reddit = praw.Reddit(client_id = 'Jzm3pXTmnfuLug',
-            client_secret = 'uL8tiUV8UOVkhgJBa7pqSAcDgTM',
-            username = 'uva_dsi_test_dev',
-            password = 'smoothtomato330',
-            user_agent = 'test-app by /u/uva_dsi_test_dev')
-        #submission.load_more_comments(limit=None, threshold=1)
+        # create Reddit object by passing credentials to Praw, get creds from creds.json
+        with open('creds.json') as config_file:
+            data = json.load(config_file)
+
+        reddit = praw.Reddit(client_id = data['client_id'],
+            client_secret = data['client_secret'],
+            username = data['user'],
+            password = data['password'],
+            user_agent = data['user_agent'])
         submission = reddit.submission(url=self.URL)
 
-        # Iterate over all of the top-level comments on the post:
+        # Iterate over all of the top-level comments on the post, get author, fliar, comment, timestamp
+        # got the keys from looking json version of the website 
+        # TO DO: figure out whether or not we are grabbing all the comments, or just the top-level ones (flatten tree?)
+        
         for comment in submission.comments:
             try:
                 analyzer = SentimentIntensityAnalyzer()
@@ -59,35 +54,50 @@ class RedditPostParse:
                 })
             except:
                 continue
-
-    #def sortFlairs(self):
-        # Obtain all flairs, split into multiple columns if there are multiple flairs, condense into one list and make that a dataframe
-    #    flair = self.postDF.flair.str.split(pat= " • " , n=-1, expand=True)
-    #    flair1 = flair[0]   #initial flair
-    #    flair2 = flair[1]   #secondary flair - displays as None if no flair exists
-    #    flair1.str.strip()
-    #    flair2.str.strip()
-    #    flairTot = flair1.append(flair2, ignore_index = True) #create one column with all flairs and then import that into a dataframe for anlaysis
-    #    flairTot = pd.DataFrame(data = flairTot, columns = ['flairs'])
-
         
     def getDataFrame(self):
+        # put everything into a dataframe
         self.postDF = pd.DataFrame(self.postDetails, columns=['author', 'flair', 'comment', 'timeStamp','sentimentScore'])
         self.postDF['timeStamp'] = pd.to_datetime(self.postDF['timeStamp'])
         self.postDF = self.postDF.sort_values(by='timeStamp',ascending=True)
         
-        self.flair1DF = self.postDF[self.postDF['flair'].str.contains(self.flair1, na = False)]
-        self.flair2DF = self.postDF[self.postDF['flair'].str.contains(self.flair2, na = False)]
+        # not sure how to handle the flairs atm. do we create the dataframe based on the flairs within the class?
+#        self.flair1DF = self.postDF[self.postDF['flair'].str.contains(self.flair1, na = False)]
+#        self.flair2DF = self.postDF[self.postDF['flair'].str.contains(self.flair2, na = False)]
         
         return self.postDF
 
-CFB = RedditPostParse("https://www.reddit.com/r/CFB/comments/9zzk5n/", 'michigan', 'ohiostate')
-CFB.getComments()
-cfb = CFB.getDataFrame()
+uofm_osu_firsthalf = RedditPostParse("https://www.reddit.com/r/CFB/comments/9zzk5n/game_thread_michigan_ohio_state_1200pm_et/", 'michigan', 'ohiostate')
+uofm_osu_firsthalf.getComments()
+uofm_osu_firsthalf_df = uofm_osu_firsthalf.getDataFrame()
 
-michiganComments = CFB.flair1DF
+uofm_osu_secondhalf = RedditPostParse("https://www.reddit.com/r/CFB/comments/a018xs/game_thread_michigan_ohio_state_1200pm_et_second/", 'michigan', 'ohiostate')
+uofm_osu_secondhalf.getComments()
+uofm_osu_secondhalf_df = uofm_osu_secondhalf.getDataFrame()
+
+uofm_osu = pd.concat([uofm_osu_firsthalf_df, uofm_osu_secondhalf_df])
+
+uofm_osu.to_csv('uofm_osu.csv') #exports results to a csv
+
+michiganComments = uofm_osu[uofm_osu['flair'].str.contains(":michigan:", na = False)]
+osuComments = uofm_osu[uofm_osu['flair'].str.contains(":ohiostate:", na = False)]
 michiganComments.head()
-osuComments = CFB.flair2DF
 osuComments.head()
 
-cfb.to_csv('CFB.csv') #exports results to a csv
+
+uofm_msu_firsthalf = RedditPostParse("https://www.reddit.com/r/CFB/comments/9puso8/game_thread_michigan_michigan_state_1200pm_et/", 'michigan', 'michiganstate')
+uofm_msu_firsthalf.getComments()
+uofm_msu_firsthalf_df = uofm_msu_firsthalf.getDataFrame()
+
+uofm_msu_secondhalf = RedditPostParse("https://www.reddit.com/r/CFB/comments/9pwu6h/game_thread_michigan_michigan_state_1200pm_et/", 'michigan', 'michiganstate')
+uofm_msu_secondhalf.getComments()
+uofm_msu_secondhalf_df = uofm_msu_secondhalf.getDataFrame()
+
+uofm_msu = pd.concat([uofm_msu_firsthalf_df, uofm_msu_secondhalf_df])
+
+uofm_msu.to_csv('uofm_msu.csv') #exports results to a csv
+
+uofMComments = uofm_msu[uofm_msu['flair'].str.contains(":michigan:", na = False)]
+msuComments = uofm_msu[uofm_msu['flair'].str.contains(":michiganstate:", na = False)]
+uofMComments.head()
+msuComments.head()
