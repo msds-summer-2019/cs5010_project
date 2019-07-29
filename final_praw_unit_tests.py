baa01c84496d80c60aaf99f5efcb2fb48869b4ee
Aditi Rajagopal <aditi.rajagopal92@gmail.com>
2019-07-29 18:53:55 -0400

    Add initial unit test suite
    Comment out code in final_project_praw.py that generates the word cloud etc before running (eventually move this code out to a different file).
    
    TO DO: Add more unit tests?

---
+++ final_praw_unit_tests.py
@@ -0,0 +1,45 @@
+"""
+
+CS5010: Final Project
+Names: Aditi Rajagopal, Bradley Katcher, Charlie Putnam
+Computing-ID: ar5vt, bk5pu, cmp2cz
+Content: Unit Test Suite for final_project_praw.py
+Notes: Comment out code in final_project_praw.py that generates the word cloud etc before running
+TO DO: Move word cloud, df generated code from final_project_praw.py to different file, add more tests?
+"""
+
+from final_project_praw import RedditPostParse
+import unittest
+
+#inherits from unittest class
+class TestRedditPostParse(unittest.TestCase): 
+
+    def test_num_posts(self):
+      redditComments = RedditPostParse("https://www.reddit.com/r/CFB/comments/cejgo2/jim_harbaugh_is_fine_urban_meyer_isnt_going_to/", 'michigan', 'ohiostate')
+      redditComments.getComments()
+      self.assertEqual(len(redditComments.postDetails), 18)      
+
+    def test_sentiment_score_type(self):
+      redditComments = RedditPostParse("https://www.reddit.com/r/CFB/comments/cejgo2/jim_harbaugh_is_fine_urban_meyer_isnt_going_to/", 'michigan', 'ohiostate')
+      redditComments.getComments()
+      self.assertIsInstance(type(redditComments.postDetails[0]['sentimentScore']), dict.__class__)
+
+    def test_comment_type(self):
+      redditComments = RedditPostParse("https://www.reddit.com/r/CFB/comments/cejgo2/jim_harbaugh_is_fine_urban_meyer_isnt_going_to/", 'michigan', 'ohiostate')
+      redditComments.getComments()
+      self.assertIsInstance(type(redditComments.postDetails[0]['comment']), str.__class__)
+
+    def test_df_size(self):
+      redditComments = RedditPostParse("https://www.reddit.com/r/CFB/comments/cejgo2/jim_harbaugh_is_fine_urban_meyer_isnt_going_to/", 'michigan', 'ohiostate')
+      redditComments.getComments()
+      commentDF = redditComments.getDataFrame()
+      self.assertEqual(commentDF.shape, (18, 11))
+
+    def test_df_cols(self):
+      redditComments = RedditPostParse("https://www.reddit.com/r/CFB/comments/cejgo2/jim_harbaugh_is_fine_urban_meyer_isnt_going_to/", 'michigan', 'ohiostate')
+      redditComments.getComments()
+      commentDF = redditComments.getDataFrame()
+      self.assertEqual(list(commentDF.columns), ['author', 'flair', 'comment', 'timeStamp', 'textblobScore', 'votes', 'flair_clean', 'neg', 'neu', 'pos', 'compound'])
+
+if __name__ == '__main__':
+    unittest.main()
